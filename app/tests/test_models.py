6243ba10cadaab3bfbc05b4af59c493ee26932c7
root <root@ubuntu.(none)>
2013-11-25 03:28:27 -0300

    Another test

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -2,7 +2,7 @@ import unittest
  
 class SimpleestCase(unittest.TestCase):
 	def test_index(self):
-		self.assertEqual(1 + 1, 2)
+		self.assertEqual(1 + 2, 3)
  
 if __name__ == '__main__':
 	unittest.main()

09d47801ede0061f887904f1dd296e13a79aeb96
Matheus <its.matheus3@gmail.com>
2013-11-30 20:57:24 -0300

    vai funcionar

--- depencencies.sh
+++ depencencies.sh
@@ -8,24 +8,24 @@
 # @desc: 
 # ----------------------------------
 
-echo "[\/] Entering virtual environment..."
+#echo "[\/] Entering virtual environment..."
 . $HOME/.virtualenvs/hi/bin/activate
 
-echo "[>] Installing Django v.1.5.2..."
+#echo "[>] Installing Django v.1.5.2..."
 pip install Django==1.5.2
 
-echo "[>] Installing Grapelli v.1.5.2..."
+#echo "[>] Installing Grapelli v.1.5.2..."
 pip install django-grappelli==2.4.6
 
-echo "[>] Installing South v.1.5.2..."
+#echo "[>] Installing South v.1.5.2..."
 pip install South==0.8.2
 
-echo "[>] Installing Unipath v.1.5.2..."
+#echo "[>] Installing Unipath v.1.5.2..."
 pip install Unipath==1.0
 
-echo "[>] Installing PostgreSQL dependencies..."
+#echo "[>] Installing PostgreSQL dependencies..."
 pip install psycopg2
 pip install setproctitle
 
-echo "[\/] Showing project dependencies!"
+#echo "[\/] Showing project dependencies!"
 pip freeze
